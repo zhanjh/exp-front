44af9a0040d61cc209432911d13d04a1139191ee
zjh <zhanjh@126.com>
2019-07-08 13:38:51 +0800

    HomePage.Event

--- src/js/web/app/landing/home.js
+++ src/js/web/app/landing/home.js
@@ -1,7 +1,8 @@
 import {HomePage} from './page/HomePage';
+import {browser_router} from 'global';
 
 const homePage = new HomePage();
-
+homePage.on(HomePage.Event.click, () => browser_router().navigate('contact.list'));
 
 export default async () => {
   return homePage;
--- src/js/web/app/landing/page/HomePage.js
+++ src/js/web/app/landing/page/HomePage.js
@@ -1,5 +1,8 @@
 import {View} from 'gap/View';
-import {browser_router} from 'global';
+
+const Event = {
+  click: 'click'
+};
 
 export class HomePage extends View {
   constructor(props) {
@@ -12,11 +15,21 @@ export class HomePage extends View {
   template() {
     return this.html`
       <div class="home">
-        <a href="#contact">
+        <a 
+          on-click=${() => this.triggerClick()}
+          href="javascript:;">
           <img src="https://www.expediagroup.com/wp-content/uploads/2018/03/Expedia-Group-Logo_E-Horizontal-Logo-Sticky.png" srcset="https://www.expediagroup.com/wp-content/uploads/2018/03/Expedia-Group-Logo_E-Horizontal-Logo-Sticky.png 1x" alt="Expedia Group Logo" data-retina_logo_url="" class="fusion-sticky-logo" data-logo-height="145" data-logo-width="330" style="" width="700" height="307">
         </a>
       </div>
     `;
+  }
+
+  onClick(fun) {
+    this.on(Event.click, fun);
+  }
+
+  triggerClick() {
+    this.trigger(Event.click);
   }
 
   adjustSize() {
@@ -24,8 +37,6 @@ export class HomePage extends View {
     this.ctn.style.height = winHeight + 'px';
     this.ctn.style.lineHeight = winHeight + 'px';
   }
-
-  gotoContactList() {
-    browser_router().navigate('contact.list');
-  }
 }
+
+HomePage.Event = Event;
